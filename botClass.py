6f84ead9fe271a1edca13b6cb214f6fcfb05981c
knutria <knox.isus@gmail.com>
2022-12-20 15:16:39 -0500

    Hola Windows

---
+++ botClass.py
@@ -0,0 +1,181 @@
+import os
+import csv
+import math
+import cv2 as cv
+import pyautogui as pt
+import pyscreenshot as ImageGrab
+from PIL import Image
+
+import pytesseract
+pytesseract.pytesseract.tesseract_cmd = r"""C:\Program Files (x86)\Tesseract-OCR\tesseract.exe"""
+
+from time import sleep
+from random import randint
+
+from coordinates import *
+from runas.runas import *
+
+def set_position(runas):
+    positions = RUNA_POSITIONS
+    i = 0
+    while i < len(runas):
+        runas[i].POSITION = positions[i]
+        i = i + 1
+
+def cleaner():
+    os.system("rm *.png")
+
+def capture_screen(window_size=WINDOW_SIZE):
+    img = ImageGrab.grab(bbox=window_size)
+    img.save("stat_windows_img.png")
+    return cv.imread("stat_windows_img.png", 0)
+
+def nav_position(position):
+    #pt.moveTo(position[0], position[1], duration = 0.1)
+    sleep(0.3)
+
+def single_click():
+    #pt.click()
+    sleep(0.3)
+
+def double_click():
+    #pt.doubleClick()
+    sleep(0.3)
+
+def nav_forge_button_position():
+    position = (FORGE_BUTTON_POSITION[0] + randint(-9,9), FORGE_BUTTON_POSITION[1] + randint(-6,6))
+    nav_position(position=position)
+
+def forge_runa():
+    print("         click")
+    single_click()
+
+def discard_runa():
+    position = (DISCARD_RUNA_POSITION[0] + randint(-9,9), DISCARD_RUNA_POSITION[1] + randint(-9,9))
+    nav_position(position=position)
+    double_click()
+
+def select_runa(runa_position):
+    position = (runa_position[0] + randint(-3,3), runa_position[1] + randint(-3,3))
+    nav_position(position=position)
+    double_click()
+
+def load_stats_from_csv(filename):
+    file = open(filename)
+    csvreader = csv.reader(file)
+    rows = []
+    for row in csvreader:
+        rows.append(row)
+    file.close()
+    return rows
+
+def img_debug(img):
+    winname = "Dofu Window"
+    cv.namedWindow(winname)
+    cv.imshow(winname, img)
+    cv.waitKey()
+
+def resize(cvImage, factor):
+    new_size = tuple(map(lambda x: x * factor, cvImage.shape[::-1]))
+    return cv.resize(cvImage, new_size)
+
+def stat_from_csv(runas, runaName):
+    for runa in runas:
+        if runa[0] == runaName:
+            return int(runa[1])
+
+def stat_from_window(runa):
+    if "ALA RESISTENCIA" in runa.NAME or "RESIS" in runa.NAME:
+        probabilidad = 0.9
+    else:
+        probabilidad = 0.70
+    
+    window = capture_screen()
+    needle = cv.imread(runa.STAT_IMG, 0)
+    result = cv.matchTemplate(window, needle, cv.TM_CCOEFF_NORMED)
+    min_val, max_val, min_loc, max_loc = cv.minMaxLoc(result)
+
+    #img_debug(window)
+    #print(min_val, max_val, min_loc, max_loc)
+
+    if max_val < probabilidad:
+        return 0
+
+    w = needle.shape[1]
+    h = needle.shape[0]
+    xloc, yloc = max_loc[0], max_loc[1]
+
+    if runa.SIGNO:
+        x_init = 17
+    else:
+        x_init = 0
+    
+    if "AUMENTO DANIO" in runa.NAME:
+        x_init = w - 2
+        xloc = x_init + 25
+    
+    minWindow = window[yloc:yloc + h, x_init:xloc]
+    img = resize(minWindow, 6)
+    #img_debug(img)
+    number = pytesseract.image_to_string(img, lang='eng', config='--oem 3 --psm 6')
+
+    try:
+        number = int(number)
+    except:
+        number = 0
+
+    return number
+
+def get_intentos(runas, runaTarget):
+    statFromCsv = stat_from_csv(runas=runas, runaName=runaTarget.NAME)
+    statFromWindow = stat_from_window(runa=runaTarget)
+    intentos = int(math.ceil((statFromCsv-statFromWindow)/runaTarget.CANT))
+    if intentos > 0:
+        if intentos > 6:
+            intentos = randint(3, 6)
+    else:
+        intentos = 0
+    print(" ", runaTarget.NAME+":", statFromCsv, statFromWindow, "   -->", intentos, "intentos")
+    return intentos
+
+def forge_runa_low(runas, runa):
+    intentos = get_intentos(runas, runa)
+    if intentos > 0:
+        discard_runa()
+        select_runa(runa_position=runa.POSITION)
+        nav_forge_button_position()
+        intento = 0
+        while intento < intentos:
+            forge_runa()
+            intento = intento + 1
+        return True
+    else:
+        return False
+
+def forge_obj(stats, runas):
+    runa = 0
+    while runa < len(runas):
+        if forge_runa_low(stats, runas[runa]):
+            runa = 0
+        else:
+            runa = runa + 1
+
+def maguear_blite():
+    runas = [RUNA_DANIO(), RUNA_CRITICO(), RUNA_SABIDURIA(), RUNA_ALA_RESISTENCIA_TIERRA()]
+    set_position(runas)
+    statsTarget = load_stats_from_csv("stats/stats_blite.csv")
+    forge_obj(statsTarget, runas)
+
+    runas = [RUNA_DANIO(), RUNA_CRITICO(), RUNA_SABIDURIA(), RUNA_RESIS_TIERRA()]
+    set_position(runas)
+    statsTarget = load_stats_from_csv("stats/stats_blite.csv")
+    forge_obj(statsTarget, runas)
+
+    runas = [RUNA_DANIO(), RUNA_CRITICO(), RUNA_SABIDURIA(), RUNA_INICIATIVA(), RUNA_PP(), 
+                RUNA_VITALIDAD(), RUNA_INTELIGENCIA(), RUNA_SUERTE()]
+    set_position(runas)
+    statsTarget = load_stats_from_csv("stats/stats_blite.csv")
+    forge_obj(statsTarget, runas)
+
+maguear_blite()
+#capture_screen()
---
+++ runas/runas.py
@@ -0,0 +1,188 @@
+class RUNA_ALA_RESISTENCIA_AGUA():
+    def __init__(self):
+        self.CANT = 1
+        self.SIGNO = True
+        self.NAME = "ALA_RESISTENCIA_AGUA"
+        self.STAT_IMG = "img/ala_resis_agua.png"
+        self.RESTOS = False
+        self.PRIMERA_RONDA = True
+
+class RUNA_ALA_RESISTENCIA_FUEGO():
+    def __init__(self):
+        self.CANT = 1
+        self.SIGNO = True
+        self.NAME = "ALA_RESISTENCIA_FUEGO"
+        self.STAT_IMG = "img/ala_resis_fuego.png"
+        self.RESTOS = False
+        self.PRIMERA_RONDA = True
+
+class RUNA_ALA_RESISTENCIA_NEUTRAL():
+    def __init__(self):
+        self.CANT = 1
+        self.SIGNO = True
+        self.NAME = "ALA_RESISTENCIA_NEUTRAL"
+        self.STAT_IMG = "img/ala_resis_fuego.png" #####################################
+        self.RESTOS = False
+        self.PRIMERA_RONDA = True
+
+class RUNA_ALA_RESISTENCIA_TIERRA():
+    def __init__(self):
+        self.CANT = 1
+        self.SIGNO = True
+        self.NAME = "ALA_RESISTENCIA_TIERRA"
+        self.STAT_IMG = "img/ala_resis_tierra.png"
+        self.RESTOS = False
+        self.PRIMERA_RONDA = True
+
+class RUNA_ALCANCE():
+    def __init__(self):
+        self.CANT = 1
+        self.SIGNO = True
+        self.NAME = "ALCANCE"
+        self.STAT_IMG = "img/alcance.png"
+        self.RESTOS = False
+        self.PRIMERA_RONDA = True
+
+class RUNA_AUMENTO_DANIO():
+    def __init__(self):
+        self.CANT = 1
+        self.SIGNO = True
+        self.NAME = "AUMENTO_DANIO"
+        self.STAT_IMG = "img/aumento_danio.png"
+        self.RESTOS = False
+        self.PRIMERA_RONDA = True
+
+class RUNA_CRITICO():
+    def __init__(self):
+        self.CANT = 1
+        self.SIGNO = True
+        self.NAME = "CRITICO"
+        self.STAT_IMG = "img/critico.png"
+        self.RESTOS = False
+        self.PRIMERA_RONDA = True
+
+class RUNA_CURA():
+    def __init__(self):
+        self.CANT = 1
+        self.SIGNO = True
+        self.NAME = "CURA"
+        self.STAT_IMG = "img/cura.png"
+        self.RESTOS = False
+        self.PRIMERA_RONDA = True
+
+class RUNA_DANIO():
+    def __init__(self):
+        self.CANT = 1
+        self.SIGNO = True
+        self.NAME = "DANIO"
+        self.STAT_IMG = "img/danio.png"
+        self.RESTOS = False
+        self.PRIMERA_RONDA = True
+
+class RUNA_FUERZA():
+    def __init__(self):
+        self.CANT = 3
+        self.SIGNO = True
+        self.NAME = "FUERZA"
+        self.STAT_IMG = "img/fuerza.png"
+        self.RESTOS = False
+        self.PRIMERA_RONDA = True
+
+class RUNA_INICIATIVA():
+    def __init__(self):
+        self.CANT = 30
+        self.SIGNO = True
+        self.NAME = "INICIATIVA"
+        self.STAT_IMG = "img/iniciativa.png"
+        self.RESTOS = False
+        self.PRIMERA_RONDA = True
+
+class RUNA_INTELIGENCIA():
+    def __init__(self):
+        self.CANT = 3
+        self.SIGNO = True
+        self.NAME = "INTELIGENCIA"
+        self.STAT_IMG = "img/inte.png"
+        self.RESTOS = False
+        self.PRIMERA_RONDA = True
+
+class RUNA_INVOCACION():
+    def __init__(self):
+        self.CANT = 1
+        self.SIGNO = True
+        self.NAME = "INVOCACION"
+        self.STAT_IMG = "img/invo.png"
+        self.RESTOS = False
+        self.PRIMERA_RONDA = True
+
+class RUNA_PM():
+    def __init__(self):
+        self.CANT = 1
+        self.SIGNO = True
+        self.NAME = "PM"
+        self.STAT_IMG = "img/pm.png"
+        self.RESTOS = False
+        self.PRIMERA_RONDA = True
+
+class RUNA_PP_ROJA():
+    def __init__(self):
+        self.CANT = 3
+        self.SIGNO = True
+        self.NAME = "PP_ROJA"
+        self.STAT_IMG = "img/pp_roja.png"
+        self.RESTOS = False
+        self.PRIMERA_RONDA = True
+
+class RUNA_PP():
+    def __init__(self):
+        self.CANT = 1
+        self.SIGNO = True
+        self.NAME = "PP"
+        self.STAT_IMG = "img/pp.png"
+        self.RESTOS = False
+        self.PRIMERA_RONDA = True
+
+class RUNA_RESIS_NEUTRAL():
+    def __init__(self):
+        self.CANT = 1
+        self.SIGNO = False
+        self.NAME = "RESIS_NEUTRAL"
+        self.STAT_IMG = "img/resis_neutral.png"
+        self.RESTOS = False
+        self.PRIMERA_RONDA = True
+
+class RUNA_RESIS_TIERRA():
+    def __init__(self):
+        self.CANT = 1
+        self.SIGNO = False
+        self.NAME = "RESIS_TIERRA"
+        self.STAT_IMG = "img/resis_tierra.png"
+        self.RESTOS = False
+        self.PRIMERA_RONDA = True
+
+class RUNA_SABIDURIA():
+    def __init__(self):
+        self.CANT = 3
+        self.SIGNO = True
+        self.NAME = "SABIDURIA"
+        self.STAT_IMG = "img/sabi.png"
+        self.RESTOS = False
+        self.PRIMERA_RONDA = True
+
+class RUNA_SUERTE():
+    def __init__(self):
+        self.CANT = 3
+        self.SIGNO = True
+        self.NAME = "SUERTE"
+        self.STAT_IMG = "img/suerte.png"
+        self.RESTOS = False
+        self.PRIMERA_RONDA = True
+
+class RUNA_VITALIDAD():
+    def __init__(self):
+        self.CANT = 10
+        self.SIGNO = True
+        self.NAME = "VITALIDAD"
+        self.STAT_IMG = "img/vita.png"
+        self.RESTOS = False
+        self.PRIMERA_RONDA = True
