81b1b3793b125a0b09bd5c018f6f2da2d9eb5d3e
knutria <knox.isus@gmail.com>
2023-01-24 18:49:40 -0500

    auto save format

--- Runas.py
+++ Runas.py
@@ -5,6 +5,7 @@ class RUNA_ALA_RESISTENCIA_AGUA():
         self.NAME = "ALA_RESISTENCIA_AGUA"
         self.STAT_IMG = "img/ala_resis_agua.png"
 
+
 class RUNA_ALA_RESISTENCIA_FUEGO():
     def __init__(self):
         self.CANT = 1
@@ -12,12 +13,14 @@ class RUNA_ALA_RESISTENCIA_FUEGO():
         self.NAME = "ALA_RESISTENCIA_FUEGO"
         self.STAT_IMG = "img/ala_resis_fuego.png"
 
+
 class RUNA_ALA_RESISTENCIA_NEUTRAL():
     def __init__(self):
         self.CANT = 1
         self.SIGNO = True
         self.NAME = "ALA_RESISTENCIA_NEUTRAL"
-        self.STAT_IMG = "img/ala_resis_fuego.png" ### Tiene imagen erronea
+        self.STAT_IMG = "img/ala_resis_fuego.png"  # Tiene imagen erronea
+
 
 class RUNA_ALA_RESISTENCIA_TIERRA():
     def __init__(self):
@@ -26,12 +29,14 @@ class RUNA_ALA_RESISTENCIA_TIERRA():
         self.NAME = "ALA_RESISTENCIA_TIERRA"
         self.STAT_IMG = "img/ala_resis_tierra.png"
 
+
 class RUNA_ALCANCE():
     def __init__(self):
         self.CANT = 1
         self.SIGNO = True
         self.NAME = "ALCANCE"
         self.STAT_IMG = "img/alcance.png"
+
 
 class RUNA_AUMENTO_DANIO():
     def __init__(self):
@@ -40,12 +45,14 @@ class RUNA_AUMENTO_DANIO():
         self.NAME = "AUMENTO_DANIO"
         self.STAT_IMG = "img/aumento_danio.png"
 
+
 class RUNA_CRITICO():
     def __init__(self):
         self.CANT = 1
         self.SIGNO = True
         self.NAME = "CRITICO"
         self.STAT_IMG = "img/critico.png"
+
 
 class RUNA_CURA():
     def __init__(self):
@@ -54,12 +61,14 @@ class RUNA_CURA():
         self.NAME = "CURA"
         self.STAT_IMG = "img/cura.png"
 
+
 class RUNA_DANIO():
     def __init__(self):
         self.CANT = 1
         self.SIGNO = True
         self.NAME = "DANIO"
         self.STAT_IMG = "img/danio.png"
+
 
 class RUNA_FUERZA():
     def __init__(self):
@@ -68,12 +77,14 @@ class RUNA_FUERZA():
         self.NAME = "FUERZA"
         self.STAT_IMG = "img/fuerza.png"
 
+
 class RUNA_INICIATIVA():
     def __init__(self):
         self.CANT = 30
         self.SIGNO = True
         self.NAME = "INICIATIVA"
         self.STAT_IMG = "img/iniciativa.png"
+
 
 class RUNA_INTELIGENCIA():
     def __init__(self):
@@ -82,12 +93,14 @@ class RUNA_INTELIGENCIA():
         self.NAME = "INTELIGENCIA"
         self.STAT_IMG = "img/inte.png"
 
+
 class RUNA_INVOCACION():
     def __init__(self):
         self.CANT = 1
         self.SIGNO = True
         self.NAME = "INVOCACION"
         self.STAT_IMG = "img/invo.png"
+
 
 class RUNA_PM():
     def __init__(self):
@@ -96,12 +109,14 @@ class RUNA_PM():
         self.NAME = "PM"
         self.STAT_IMG = "img/pm.png"
 
+
 class RUNA_PP_ROJA():
     def __init__(self):
         self.CANT = 3
         self.SIGNO = True
         self.NAME = "PP_ROJA"
         self.STAT_IMG = "img/pp_roja.png"
+
 
 class RUNA_PP():
     def __init__(self):
@@ -110,12 +125,14 @@ class RUNA_PP():
         self.NAME = "PP"
         self.STAT_IMG = "img/pp.png"
 
+
 class RUNA_RESIS_NEUTRAL():
     def __init__(self):
         self.CANT = 1
         self.SIGNO = False
         self.NAME = "RESIS_NEUTRAL"
         self.STAT_IMG = "img/resis_neutral.png"
+
 
 class RUNA_RESIS_TIERRA():
     def __init__(self):
@@ -124,6 +141,7 @@ class RUNA_RESIS_TIERRA():
         self.NAME = "RESIS_TIERRA"
         self.STAT_IMG = "img/resis_tierra.png"
 
+
 class RUNA_SABIDURIA():
     def __init__(self):
         self.CANT = 3
@@ -131,12 +149,14 @@ class RUNA_SABIDURIA():
         self.NAME = "SABIDURIA"
         self.STAT_IMG = "img/sabi.png"
 
+
 class RUNA_SUERTE():
     def __init__(self):
         self.CANT = 3
         self.SIGNO = True
         self.NAME = "SUERTE"
         self.STAT_IMG = "img/suerte.png"
+
 
 class RUNA_VITALIDAD():
     def __init__(self):
